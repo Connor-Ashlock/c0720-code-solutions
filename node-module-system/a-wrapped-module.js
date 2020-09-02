2b9bddfa55dd724a1d4415b8f4198f0ec136a91b
connor-ashlock <connor9804@yahoo.com>
2020-09-02 23:26:54 +0000

    Completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+console.log('exports type:', typeof exports);
+console.log('require type:', typeof require);
+console.log('module type:', typeof module);
+console.log('__filename type:', typeof __filename);
+console.log('__dirname type:', typeof __dirname);
