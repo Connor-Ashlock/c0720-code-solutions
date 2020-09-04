34eadd562c4736306d34c6c136577bcacea8dd8c
connor-ashlock <connor9804@yahoo.com>
2020-09-04 00:40:42 +0000

    Made change to nested fs writefile by adding 2 to err param

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) throw err;
   fs.writeFile(process.argv[3], data, 'utf-8', err2 => {
-    if (err2) throw err;
+    if (err2) throw err2;
   });
 });
