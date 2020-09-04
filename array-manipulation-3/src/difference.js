e48d6649a3f1904156d131ba398c5bee057f5f99
connor-ashlock <connor9804@yahoo.com>
2020-09-04 04:16:48 +0000

    Completed array-manipulation-3

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,28 @@
 /* eslint-disable no-unused-vars */
+function difference(first, second) {
+  const arr = [];
+  for (let i = 0; i < first.length; i++) {
+    if (first[i]) {
+      if (!second.includes(first[i])) {
+        arr.push(first[i]);
+      }
+    }
+  }
+  for (let i = 0; i < second.length; i++) {
+    if (second[i]) {
+      if (!first.includes(second[i])) {
+        arr.push(second[i]);
+      }
+    }
+  }
+  return arr;
+}
+
+// declare a variable with an empty array
+// create for loop
+// check if current index is truthy
+// if true, check if the current index of the first param is not included in the second array
+// if true, push the current index into the new array
+// exit loop
+// create for loop and do the exact same thing, but for the second param
+// return the array
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,21 @@
 /* eslint-disable no-unused-vars */
+function union(first, second) {
+  const arr = [];
+  for (let i = 0; i < first.length; i++) {
+    arr.push(first[i]);
+  }
+  for (let i = 0; i < second.length; i++) {
+    if (!first.includes(second[i])) {
+      arr.push(second[i]);
+    }
+  }
+  return arr;
+}
+
+// declare variable with empty array
+// create for loop
+// push the first param index at i into the new array
+// exit loop and create a new one
+// check if second array at i is not included in first array
+// if true, push array at i into new array
+// return array
