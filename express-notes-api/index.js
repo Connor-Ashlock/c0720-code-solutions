7369c8479eb41bb2b5e613c113a66293b65b87fd
connor-ashlock <connor9804@yahoo.com>
2020-09-10 22:53:54 +0000

    Added id searching for get feature

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -6,12 +6,34 @@ const notes = data.notes;
 const notesArr = [];
 
 app.get('/api/notes', (req, res) => {
-  for (const prop in notes) {
-    notesArr.push(notes[prop]);
-  }
   res.json(notesArr);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  let isIdFound = false;
+  if (!Number(req.params.id) || req.params.id < 1) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else {
+    for (let i = 0; i < notesArr.length; i++) {
+      if (Number(req.params.id) === notesArr[i].id) {
+        res.json(notesArr[i]);
+        isIdFound = true;
+      }
+    }
+    if (!isIdFound) {
+      res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+    }
+  }
 });
 
 app.listen(3000, () => {
   console.log('server is listening');
 });
+
+function start() {
+  for (const prop in notes) {
+    notesArr.push(notes[prop]);
+  }
+}
+
+start();
