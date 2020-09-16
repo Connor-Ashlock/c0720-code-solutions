c4929d5fc99185f375304c7de051322bf0038a24
connor-ashlock <connor9804@yahoo.com>
2020-09-16 21:45:19 +0000

    got rid of counter props and adjusted the render method

--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -5,25 +5,19 @@ class CustomButton extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.nextCount = 1;
-    this.hotCount = 0;
     this.hotLevel = ['normal', 'dark-purp', 'light-purp', 'light-red', 'light-org', 'yellow', 'white'];
     this.state = { clickCount: 0 };
   }
 
   handleClick() {
-    this.setState({ clickCount: this.nextCount++ });
+    this.setState({ clickCount: this.state.clickCount + 1 });
   }
 
   render() {
     if (this.state.clickCount === 18) {
       return <button className={this.hotLevel[6]}>Hot Button</button>;
     }
-    return (
-      this.state.clickCount % 3 === 0
-        ? <button className={this.hotLevel[this.hotCount++]} onClick={this.handleClick}>Hot Button</button>
-        : <button className={this.hotLevel[this.hotCount - 1]} onClick={this.handleClick}>Hot Button</button>
-    );
+    return <button className={this.hotLevel[parseInt(this.state.clickCount / 3, 10)]} onClick={this.handleClick}>Hot Button</button>;
   }
 }
 
