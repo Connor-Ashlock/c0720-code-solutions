51c079b0bd282af50d36c00e9cca6fe808fb1e60
connor-ashlock <connor9804@yahoo.com>
2020-09-17 20:16:11 +0000

    Completed react-app-drawer-component

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { isClicked: false };
+  }
+
+  handleClick() {
+    this.setState({ isClicked: !this.state.isClicked });
+  }
+
+  render() {
+    if (!this.state.isClicked) {
+      return (
+        <i className="fas fa-bars" onClick={this.handleClick}></i>
+      );
+    }
+    return (
+      <div className="row">
+        <h1 className="menu" onClick={this.handleClick}>Menu</h1>
+        <p onClick={this.handleClick}>About</p>
+        <p onClick={this.handleClick}>Get Started</p>
+        <p onClick={this.handleClick}>Sign In</p>
+      </div>
+    );
+  }
+}
+
+export default AppDrawer;
