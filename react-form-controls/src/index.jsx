bdf63d79afc56879c384611be507dd49e975cc85
connor-ashlock <connor9804@yahoo.com>
2020-09-17 18:18:06 +0000

    Completed react-form-controls

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -1,13 +1,36 @@
 /* eslint-disable no-console */
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// class NewsletterForm extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { email: '' };
+  }
 
-//   render() {
+  handleChange() {
+    this.setState({ email: event.target.value });
+  }
 
-//   }
-// }
+  handleSubmit() {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Email</label>
+        <input type="email" value={this.state.email} onChange={this.handleChange} />
+        <input type="submit" value="Sign Up" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.getElementById('root')
+);
